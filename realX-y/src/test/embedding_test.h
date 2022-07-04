dfd73b3bfc0cdcc86cb7a0ccfe55ff6d86c69c10
surynek <pavel.surynek@seznam.cz>
2022-07-04 13:03:40 +0200

    ZZZZ

---
+++ realX-y/src/test/embedding_test.h
@@ -0,0 +1,46 @@
+/*============================================================================*/
+/*                                                                            */
+/*                                                                            */
+/*                             realX 0-053_nofutu                             */
+/*                                                                            */
+/*                  (C) Copyright 2021 - 2022 Pavel Surynek                   */
+/*                                                                            */
+/*                http://www.surynek.net | <pavel@surynek.net>                */
+/*       http://users.fit.cvut.cz/surynek | <pavel.surynek@fit.cvut.cz>       */
+/*                                                                            */
+/*============================================================================*/
+/* embedding_test.h / 0-053_nofutu                                            */
+/*----------------------------------------------------------------------------*/
+//
+// Virtual network embedding test.
+//
+/*----------------------------------------------------------------------------*/
+
+
+#ifndef __EMBEDDING_TEST_H__
+#define __EMBEDDING_TEST_H__
+
+#include "defs.h"
+#include "result.h"
+
+
+/*----------------------------------------------------------------------------*/
+
+namespace realX
+{
+
+void print_Introduction(void);
+
+sResult test_embedding_1(void);
+sResult test_embedding_2(void);
+sResult test_embedding_3(void);
+sResult test_embedding_4(void);
+sResult test_embedding_5(void);
+
+
+/*----------------------------------------------------------------------------*/
+
+} // namespace realX
+
+#endif /* __EMBEDDING_TEST_H__ */
+
---
+++ realX-y/src/version.h
@@ -0,0 +1,42 @@
+/*============================================================================*/
+/*                                                                            */
+/*                                                                            */
+/*                             realX 0-053_nofutu                             */
+/*                                                                            */
+/*                  (C) Copyright 2021 - 2022 Pavel Surynek                   */
+/*                                                                            */
+/*                http://www.surynek.net | <pavel@surynek.net>                */
+/*       http://users.fit.cvut.cz/surynek | <pavel.surynek@fit.cvut.cz>       */
+/*                                                                            */
+/*============================================================================*/
+/* version.h / 0-053_nofutu                                                   */
+/*----------------------------------------------------------------------------*/
+//
+// Version file for realX
+//
+/*----------------------------------------------------------------------------*/
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+using namespace realX;
+
+
+/*----------------------------------------------------------------------------*/
+
+namespace realX
+{
+
+#define sVERSION "0-053_nofutu"
+#define sPRODUCT "realX 0-053_nofutu"
+#define sAUTHOR "Pavel Surynek"
+#define sAUTHOR_LONG "Pavel Surynek <pavel@surynek.net>"
+#define sCOPYRIGHT "(C) Copyright 2021 - 2022 Pavel Surynek"
+#define sURL "http://www.surynek.net"
+
+
+/*----------------------------------------------------------------------------*/
+
+} // namespace realX
+
+#endif /* __VERSION_H__ */
