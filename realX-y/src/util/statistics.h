e93c90fd53347ab5e20e12a34cb1841e5698a2ca
surynek <pavel.surynek@seznam.cz>
2022-11-24 00:32:59 +0100

    New version of realX.

--- realX-y/src/util/statistics.h
+++ realX-y/src/util/statistics.h
@@ -1,7 +1,7 @@
 /*============================================================================*/
 /*                                                                            */
 /*                                                                            */
-/*                             realX 0-108_nofutu                             */
+/*                             realX 0-110_nofutu                             */
 /*                                                                            */
 /*                  (C) Copyright 2021 - 2022 Pavel Surynek                   */
 /*                                                                            */
@@ -9,7 +9,7 @@
 /*       http://users.fit.cvut.cz/surynek | <pavel.surynek@fit.cvut.cz>       */
 /*                                                                            */
 /*============================================================================*/
-/* statistics.h / 0-108_nofutu                                                */
+/* statistics.h / 0-110_nofutu                                                */
 /*----------------------------------------------------------------------------*/
 //
 // Statistical data collection and analytical tools.
